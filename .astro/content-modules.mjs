a6ede45a2ed0c5db0842ea077893b407aaefdf8b
Mark <mark@Marks-MacBook-Air.local>
2026-02-19 11:19:02 +1100

    Update little things

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,7 @@
+
+export default new Map([
+["src/content/blog/embed-examples.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fembed-examples.mdoc&astroContentModuleFlag=true")],
+["src/content/blog/guides/content-collections-guide.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fguides%2Fcontent-collections-guide.mdoc&astroContentModuleFlag=true")],
+["src/content/blog/guides/markdown-guide.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fguides%2Fmarkdown-guide.mdoc&astroContentModuleFlag=true")],
+["src/content/blog/guides/bloomfolio-complete-guide-building-your-portfolio.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fguides%2Fbloomfolio-complete-guide-building-your-portfolio.mdoc&astroContentModuleFlag=true")]]);
+		
